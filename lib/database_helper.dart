64ca2473b368833c72c150ffff4c28709e67f75c
Chet Wijesekera <chethana96@gmail.com>
2020-10-27 00:20:19 +1300

    Render existing tasks to the home screen

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -21,7 +21,13 @@ class DatabaseHelper {
   Future<void> insertTask(Task task) async {
     Database db = await database();
     await db.insert('tasks', task.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+  }
 
-
+  Future<List<Task>> getTasks() async {
+    Database db = await database();
+    List<Map<String, dynamic>> tasks = await db.query('tasks');
+    return List.generate(tasks.length, (index) {
+      return Task(id: tasks[index]["id"], title: tasks[index]["title"], description: tasks[index]["description"]);
+    });    
   }
 }
